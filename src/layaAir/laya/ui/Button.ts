8626d20444d0eaa690110724a56dc29a5fa20d11
DiCE <2798007640@qq.com>
2025-11-10 17:33:35 +0800

    添加按钮点击音效

--- src/layaAir/laya/ui/Button.ts
+++ src/layaAir/laya/ui/Button.ts
@@ -22,6 +22,9 @@ import { LayaEnv } from '../../LayaEnv';
  * 多种状态，可以是单态，两态（移出、按下）和三态(移出、悬停、按下)，默认是三态。
  */
 export class Button extends UIComponent implements ISelect {
+
+    static globalMouseDownHook: Handler;
+
     /**
      * @en Controls whether the button can toggle its display state. When the value is true, the display state can be toggled by clicking, such as switching between selected and unselected states.
      * @zh 用于控制按钮是否可切换显示状态；值为 true 时，才可以在运行后通过点击切换显示状态。例如选中状态和未选中状态。
